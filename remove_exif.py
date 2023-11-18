504335d512e4c69b1e38bf971f8011b16a38a75c
DevSecHacker <44221480+RajuGanapathiraju@users.noreply.github.com>
2023-11-18 10:44:03 +0530

    Add files via upload

---
+++ remove_exif.py
@@ -0,0 +1,24 @@
+import piexif
+from PIL import Image
+
+def strip_exif(file_path):
+    pil_image = Image.open(file_path)
+    exif_data = pil_image.info.get('exif')
+    
+
+    if exif_data is not None:
+        # Remove the Exif data
+        exif_dict = piexif.load(exif_data)
+        exif_dict.pop('Exif', None)
+        exif_bytes = piexif.dump(exif_dict)
+        pil_image.info['exif'] = exif_bytes
+
+    return pil_image
+
+
+if __name__ == '__main__':
+    input_file = '/Users/rgy/Downloads/exif_example.jpeg'
+    output_file = '/Users/rgy/Downloads/exif_example_without_exif.jpeg'
+
+    new_image = strip_exif(input_file)
+    new_image.save(output_file, quality=95)
